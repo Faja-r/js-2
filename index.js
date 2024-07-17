3b73f1e0149315db8e61c4a51d85fedf6991b2c3
Faja-r <143716722+Faja-r@users.noreply.github.com>
2024-07-18 00:01:03 +0500

    new

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-// index.js
 
-// Get the input box and list container elements
 const inputbox = document.getElementById("input-box");
 const listcontainer = document.getElementById("list-container");
 
-// Function to add a new task
 function addtask() {
-    // Check if input box is empty
-    if (inputbox.value === '') {
+        if (inputbox.value === '') {
         alert("You must write something");
     } else {
-        // Create a new list item
         let li = document.createElement("li");
-        li.textContent = inputbox.value; // Set the text content of the list item
-
-        // Append the new list item to the list container
+        li.innerHTML = inputbox.value; 
         listcontainer.appendChild(li);
-
-        // Clear the input box after adding task
+       
+        }
         inputbox.value = '';
-    }
 }
